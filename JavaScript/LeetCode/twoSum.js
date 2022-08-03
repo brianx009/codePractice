5e482336896d8a355ddc409ffc4ed98cb1f581d8
BrianSaldana <brianxsaldana@gmail.com>
2022-08-03 15:26:27 -0700

    added documentation to leet code problem

--- JavaScript/LeetCode/twoSum.js
+++ JavaScript/LeetCode/twoSum.js
@@ -8,10 +8,10 @@ You can return the answer in any order.
   */
 //double for loop solution, time complexity o(n)^2
 // var twoSum = function(nums, target) {
-//     for (let i = 0; i < nums.length; i++){
-//         for (let j = i + 1; j< nums.length; j++){
-//             if (nums[i] + nums[j] === target){
-//                 return [i,j];
+//     for (let i = 0; i < nums.length; i++){  //for loop that cycles through the array index i to get a number
+//         for (let j = i + 1; j< nums.length; j++){ // second for loop that cyckes through the array index[i+1] in order to not use the same number twice
+//             if (nums[i] + nums[j] === target){ // if number [i] + num[j] = target value then
+//                 return [i,j]; // return the index of [i] and [j]
 //             }
 //         }
 //     }
@@ -22,15 +22,15 @@ You can return the answer in any order.
 
 //optimized solution, time complexity o(n)
 var twoSum = function(nums, target) {
-    const hashtable = {};
+    const hashtable = {};  //creating a hashtable to store our keys and their values
 
-    for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
+    for(let i = 0; i < nums.length; i++){ //for loop that will cycle throught the keys in our hashtable
+        let complement = target - nums[i]; // setting our key "complememnt" = the target - a number in the index
 
-        if (hashtable.hasOwnProperty(complement)){
-            return [hashtable[complement], i];
+        if (hashtable.hasOwnProperty(complement)){ // if the key exists in the hashtable
+            return [hashtable[complement], i]; //then we will return the index of the key by accessing hashtable, and returning the index of the value i
         }
-        hashtable[nums[i]] = i;
+        hashtable[nums[i]] = i; // else if the number doesn't exist in our hashtable we are going to add it to our hashtable in the format "key:i" or "complement: i"
     }
 };
 const test1 = twoSum([1,2,3,4,5], 7);
