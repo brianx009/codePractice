4614b4f35e279adee89c796ea066dbe99f9b85be
BrianSaldana <brianxsaldana@gmail.com>
2022-09-20 16:42:56 -0700

    Inroduction to Data Structures

---
+++ C++/DS&A/introductionToDS/intro.cpp
@@ -0,0 +1,16 @@
+/*
+A data structure is a way to store and organize data in a computer, so that it canbe used efficiently.
+
+We can talk about data structures as:
+1) Mathematical/ logical models or also known as Abstract Data Types
+Abstract Data Types: define data and operations but no implementation
+    ex of Abstract Data Type:
+        List:
+            - stores a given number of elements of any type
+            - reads elements by position
+            - modifies an element at a given postion
+2) Implementation 
+    ex of Concrete Implementation:
+        Arrays
+        Linked List
+*/
