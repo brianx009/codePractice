2389c0487df2e13a4696afbae62cbabd5d41307b
BrianSaldana <brianxsaldana@gmail.com>
2022-09-20 16:29:55 -0700

    Hello World in C++

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,10 @@
+//first C++ program that outputs Hello World! 
+// run in terminal by running .\a.out or .\a.exe
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+};
